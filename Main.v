e2f93b5ffe311233e117dc87173f41a0f6a54622
PaulMCarrillo <72674231+PaulMCarrillo@users.noreply.github.com>
2020-10-14 17:28:38 -0500

    Update Main.v

--- Main.v
+++ Main.v
@@ -12,20 +12,27 @@ module HELPv2
 (
             input clk,
             input SW0,
-            output LED0,
             input SW1,
-            output LED1,
             input SW2,
-            output LED2,
             input SW3,
-            output LED3,
             input SW4,
-            output LED4,
             input SW5,
-            output LED5,
             input SW6,
-            output LED6,
             input SW7,
+            output LED0,
+            output LED1,
+            output LED2,
+            output LED3,
+            output LED4,
+            output LED5,
+            output LED6,
+            output LED7,
+            output LED8,
+            output LED9,
+            input JA0,
+            input JA1,
+            input JA2,
+            input JA3,
             input JB0,
             input JB1,
             output JC0,
@@ -34,8 +41,6 @@ module HELPv2
             output JC4,
             output JC5,
             output JC6,
-            output LED7,
-            input JA0,
             output reg [3:0] an,      // 4 Digits on Basys 3 Board
             output reg [6:0] seg    // 7 Segment Display 
  );
@@ -48,14 +53,19 @@ assign LED2 = SW2;
 assign LED3 = SW3;
 assign LED4 = SW4;
 assign LED5 = SW5;
-assign LED6 = SW6;
 
 //H-bridge outputs tied to switches
-assign JC1 = SW3;
-assign JC2 = SW4;
-assign JC5 = SW4;
-assign JC6 = SW3;
-assign JA0 = LED7;
+//assign JC1 = SW3;
+//assign JC2 = SW4;
+//assign JC5 = SW4;
+//assign JC6 = SW3;
+
+//LEDs to test IPS functionality
+assign LED6 = ~JA0;
+assign LED7 = ~JA1;
+assign LED8 = ~JA2;
+assign LED9 = ~JA3;
+
 
  //USED FOR 7-seg, DON'T CHANGE!!!!!!!!
  //Use the 2 MSBs of 19-bit counter to create 190 Hz frequency refresh
@@ -69,6 +79,7 @@ assign JA0 = LED7;
 
 
 //7-Segment bullshit. Literally just collapse this and ignore it.
+//This shit needs to be overhauled for the main project... we'll get to it
 //DO NOT CHANGE UNLESS YOU ARE 100% CERTAIN WHAT YOU ARE DOING
 //EVEN THEN, CONSULT ME (PAUL) BEFORE CHANGING!!!!!!!!
 always @ (*) 
@@ -179,22 +190,25 @@ always @ (posedge clk)
 wire SP1C;
     assign SP1C = (speedcounter1 < 23550) ? 1:0;
     
+//Old code for intro project - ignore  
 //Then AND the PWM with SW0 
-wire SP1E;
-    and(SP1E, SP1C, SW0);
+//wire SP1E;
+//    and(SP1E, SP1C, SW0);
 
 //Wire SP2C is 1 when the speedcounter1 < 26550, and 0 when speedcounter1 > 26550
 //This creates a 71% duty cycle, creating speed 2.
 wire SP2C;
     assign SP2C = (speedcounter2 < 26555) ? 1:0;
     
+//Old code for intro project - ignore    
 //Then AND the PWM with SW1 
-wire SP2E;
-    and(SP2E, SP2C, SW1);
+//wire SP2E;
+//    and(SP2E, SP2C, SW1);
 
+//Old code for intro project - ignore
 //Wire SP3C is just switch 2, no duty cycle  
-wire SP3E;
-    assign SP3E = SW2;
+//wire SP3E;
+    //assign SP3E = SW2;
 
 //This was for overcurrent protection but we never got that shit working  
 //reg [30:0] speedcounter3;
@@ -210,23 +224,166 @@ wire SP3E;
 //always @ (posedge stop)
 //JB0_last = ~JB0;
 
-//Wire SPSel is XOR'd with all 3 SP#E wires
-//This ensures only 1 speed is selected on a time
-wire SPSel;
-    xor(SPSel, SP1E, SP2E, SP3E); 
+    
+//4 bit register, each ips controlls 1 bit
+reg [3:0] IPSDetection;
+always @ (*)
+	begin
+	    //left outer sensor
+	    IPSDetection[3] = ~JA0;
+	    //left inner sensor
+	    IPSDetection[2] = ~JA1;
+	    //right inner sensor 
+		IPSDetection[1] = ~JA2;
+		//right outer sensor
+		IPSDetection[0] = ~JA3;
 
-//IPS inputs - work in progress 
-wire IPS;
-    assign IPS = ~JA0; 
+	end
 
-//AND gate with IPS and SPSel to drive ENA and ANB
-//ENA and ENB essentially act as power to the rover
-wire OnCondition;
-    and(OnCondition, IPS, SPSel);
+//register to control each side's motor speed
+reg [1:0] MotorSides;
+//Left side
+assign JC0 = MotorSides[1]; 
+//Right side
+assign JC4 = MotorSides[0]; 
 
-//ENA and ENB are driven by OnCondition
-assign JC0 = OnCondition; 
-assign JC4 = OnCondition;    
+reg [3:0] MotorPolarity = 4'b0000;
+assign JC6 = MotorPolarity[0]; //right side forward
+assign JC5 = MotorPolarity[1]; //right side backward
+assign JC1 = MotorPolarity[2]; //left side forward
+assign JC2 = MotorPolarity[3]; //left side backward
+
+//Motor movement patterns
+//I'm not sommenting on all this shit
+always @ (*)
+    begin
+        case(IPSDetection)
+            4'b0000: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                    MotorPolarity[2] = 0;
+                    MotorPolarity[0] = 0;
+                    MotorPolarity[1] = 1;
+                    MotorPolarity[3] = 1;
+                end
+            4'b0001: 
+                begin
+                    MotorSides[1] = SP2C;
+                    MotorSides[0] = SP1C;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 0;
+                    MotorPolarity[1] = 1;
+                    MotorPolarity[3] = 0;
+                end
+            4'b0010: 
+                begin
+                    MotorSides[1] = 1;
+                    MotorSides[0] = 0;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[3] = 0;
+                end
+            4'b0011: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 0;
+                    MotorPolarity[1] = 1;
+                    MotorPolarity[3] = 0;
+                end
+            4'b0100: 
+                begin
+                    MotorSides[1] = 0;
+                    MotorSides[0] = 1;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[3] = 0;
+                end
+            4'b0101: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                end
+            4'b0110: 
+                begin
+                    MotorSides[1] = SP2C;
+                    MotorSides[0] = SP2C;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[3] = 0;
+                end
+            4'b0111: 
+                begin
+                    MotorSides[1] = 1;
+                    MotorSides[0] = 1;
+                    MotorPolarity[1] = 1;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 0;
+                    MotorPolarity[3] = 0;
+                end
+            4'b1000: 
+                begin
+                    MotorSides[1] = SP2C;
+                    MotorSides[0] = 1;
+                    MotorPolarity[2] = 0;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[3] = 1;
+                end
+            4'b1001: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                end
+            4'b1010: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                end
+            4'b1011: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP2C;
+                end
+            4'b1100: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                    MotorPolarity[2] = 0;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[3] = 1;
+                end
+            4'b1101: 
+                begin
+                    MotorSides[1] = SP2C;
+                    MotorSides[0] = SP1C;
+                end
+            4'b1110: 
+                begin
+                    MotorSides[1] = SP2C;
+                    MotorSides[0] = SP2C;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[3] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[2] = 0;
+                end
+            4'b1111: 
+                begin
+                    MotorSides[1] = SP1C;
+                    MotorSides[0] = SP1C;
+                    MotorPolarity[2] = 1;
+                    MotorPolarity[0] = 1;
+                    MotorPolarity[1] = 0;
+                    MotorPolarity[3] = 0;
+                end  
+        endcase
+    end
 
 //End of module - hooray!!!
 //There's a way to use multiple modules in one file, but my ass hasn't figured it out yet
